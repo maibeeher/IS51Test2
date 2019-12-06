b72f55246f4f8de99337fe632bb866ca8fa76185
unknown <maibeeher@mail.fresnostate.edu>
2019-12-05 17:39:44 -0800

    Finish Test2

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,7 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
-
+export interface Testscore {
+  id: string;
+  testName: string;
+  pointsPossible: number;
+  pointsReceived: number;
+  percentage: number;
+  grade: number;
+}
 @Component({
   selector: 'app-test-score',
   templateUrl: './test-score.component.html',
@@ -12,7 +19,7 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<Testscore> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -21,7 +28,42 @@ export class TestScoreComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+  }
+
+  async loadTestFromJSON() {
+    const tests = await this.http.get('assets/tests.json').toPromise();
+    return tests.json();
+  }
+
+addTest() {
+  const test: Testscore = {
+    id: null,
+    testName: null,
+    pointsPossible: null,
+    pointsReceived: null,
+    percentage: null,
+    grade: null
+
+  };
+  this.tests.unshift(test);
+  this.saveToLocalStorage();
+}
+
+  deleteTests(index: number) {
+    this.tests.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('Tests', JSON.stringify(this.tests));
+    this.toastService.showToast('success', 5000, 'Success! Items saved!');
 
   }
+
+  search(paramas: string) {
+    this.tests = this.tests.filter((contact: Testscore) => {
+    });
+  }
+
 
 }
